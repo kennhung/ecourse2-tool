a25972c9185241bb29665722a5768dbaaa03e931
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2020-09-29 22:21:11 +0800

    add main

--- submitAttendence.py
+++ submitAttendence.py
@@ -4,29 +4,27 @@ from getpass import getpass
 from constant import baseURL
 from login import login, getSession
 
-username = ""
-password = ""
+def submitAttend(session, sessid, studentPassword):
+    r = session.get(
+        baseURL + "/mod/attendance/attendance.php?sessid={sessid}".format(sessid=sessid))
+    soup = BeautifulSoup(r.text, 'html.parser')
+    sessKey = soup.find('input', {"name": "sesskey"})["value"]
+    mform_isexpanded_id_session = soup.find(
+        'input', {"name": "mform_isexpanded_id_session"})["value"]
+    _qf__mod_attendance_student_attendance_form = soup.find(
+        'input', {"name": "_qf__mod_attendance_student_attendance_form"})["value"]
+    return session.post(baseURL + "/mod/attendance/attendance.php", headers={'Content-Type': "application/x-www-form-urlencoded"},
+                        data="sessid={sessid}&sesskey={sesskey}&mform_isexpanded_id_session={mform_isexpanded_id_session}".format(sessid=sessid, sesskey=sessKey, mform_isexpanded_id_session=mform_isexpanded_id_session) +
+                        "&_qf__mod_attendance_student_attendance_form={qf}".format(qf=_qf__mod_attendance_student_attendance_form) +
+                        "&studentpassword={stuPassword}&status=5".format(stuPassword=studentPassword))
 
-session = getSession()
-login(username, password, session)
 
-sessid = ""
-studentPassword = ""
+if __name__ == "__main__":
+    username = input("username: ")
+    password = input("password: ")
+    session = getSession()
+    login(username, password, session)
 
-r = session.get(
-    baseURL + "/mod/attendance/attendance.php?sessid={sessid}".format(sessid=sessid))
-
-soup = BeautifulSoup(r.text, 'html.parser')
-
-sessKey = soup.find('input', {"name": "sesskey"})["value"]
-mform_isexpanded_id_session = soup.find(
-    'input', {"name": "mform_isexpanded_id_session"})["value"]
-_qf__mod_attendance_student_attendance_form = soup.find(
-    'input', {"name": "_qf__mod_attendance_student_attendance_form"})["value"]
-
-r = session.post(baseURL + "/mod/attendance/attendance.php", headers={'Content-Type': "application/x-www-form-urlencoded"},
-                 data="sessid={sessid}&sesskey={sesskey}&mform_isexpanded_id_session={mform_isexpanded_id_session}".format(sessid=sessid, sesskey=sessKey, mform_isexpanded_id_session=mform_isexpanded_id_session) +
-                 "&_qf__mod_attendance_student_attendance_form={qf}".format(qf=_qf__mod_attendance_student_attendance_form) +
-                 "&studentpassword={stuPassword}&status=5".format(stuPassword=studentPassword))
-
-print(r)
+    sessid = input("sessid: ")
+    studentPassword = input("studentPassword: ")
+    submitAttend(session, sessid, studentPassword)
